a312f09a0d9cae6e2bf35aa684a5ef409b2ed71b
Zjhyang12335 <anilyca@outlook.com>
2023-04-07 12:09:44 +0800

    4.7.2完成头像上传

---
+++ library/function.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * 为头像生成缩略图……
+ * $max_width,$max_height缩略图的最大宽度和高度
+ * $file_path原图存放路径
+ * $save_path缩略图存放路径
+*/
+function thumb($max_width,$max_height,$file_path,$save_path){
+    //通过getimagesize()获取图像信息
+    //该函数参数接收图片文件的路径，返回值为图像信息数组
+    //在图像信息数组中，前两个数组元素保存了图片的宽度和高度
+    //为上传头像生成缩略图   
+    list($width, $height) = getimagesize($file_path);
+    //等比例计算缩略图的宽和高
+    if($width/$max_width > $height/$max_height) {
+        //宽度大于高度时，将宽度限制为最大宽度，然后计算高度值
+        $new_width = $max_width;
+        $new_height = round($new_width / $width * $height);
+    }else{
+        //高度大于宽度时，将高度限制为最大高度，然后计算宽度值
+        $new_height = $max_height;
+        $new_width = round($new_height / $height * $width);
+    }
+    //绘制缩略图的画布资源
+    $thumb = imagecreatetruecolor($new_width, $new_height);
+    //从文件中读取出图像，创建为jpeg格式的图像资源
+    $source = imagecreatefromjpeg($file_path);
+    //将原图缩放填充到缩略图画布中
+    imagecopyresized($thumb,$source,0,0,0,0,$new_width,$new_height,$width,$height);
+    //将保存缩略图到指定目录（参数依次为图像资源、保存目录、输出质量0~100）
+    imagejpeg($thumb, $save_path, 100);
+}
+
--- photo.php
+++ photo.php
@@ -1,14 +1,28 @@
 <?php
+require './library/function.php';
 header('content-type:text/html; charset=utf-8');
-$user_id = 1;
+$user_id = 21180123;
+// 定义缩略图存放路径
+$photo = "./uploads/thumb_$user_id.jpg";
+// 判断是否上传图像
 if (isset($_FILES['pic'])){
     // 获取文件的信息
     $pic = $_FILES['pic'];
     // 设置图像文件存储的路径
     $save_path = "./uploads/$user_id.jpg";
+    // 判断文件类型是否正确
+    // 通过文件的MIME信息进行判断
+    if ($pic['type'] !== 'image/jpeg'){
+        exit('图像类型不符，只支持jpg格式');
+    }
+    if ($pic['size'] > 2000000){
+        exit('图像大小超过2M，上传失败');
+    }
     // 将上传的文件从临时目录中复制到指定路径
     if (!move_uploaded_file($pic['tmp_name'], $save_path)){
         exit('上传头像失败');
     }
+    // 保存成功，调用函数为头像生成缩略图
+    thumb(150, 150, $save_path, $photo);
 }
 require './view/photo.html';
